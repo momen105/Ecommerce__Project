64b2130cc185fb7cf7f3b24ae3db840a9c88ba12
Tohin <momentohin02@gmail.com>
2021-07-12 11:42:35 +0600

    media urls changed

--- Ecommerce_Project/urls.py
+++ Ecommerce_Project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path , include
+from django.conf import settings
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 # To show media files
-from django.conf import settings
-from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+#from django.conf import settings
+#from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,7 +13,10 @@ urlpatterns = [
     path('account/', include('Login_App.urls')),
     path('shop/', include('Order_App.urls')),
     path('payment/', include('Payment_App.urls')),
+
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += staticfiles_urlpatterns()
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
